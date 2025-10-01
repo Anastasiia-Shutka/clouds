190ba3684e2aad8a77eb6f46270cafdbffb82293
Anastasiia-Shutka <anastasiia.shutka@gmail.com>
2025-10-01 17:03:09 +0300

    whatever

--- lab5/app/my_project/sportsman/domain/coaches_contact.py
+++ lab5/app/my_project/sportsman/domain/coaches_contact.py
@@ -1,21 +1,19 @@
 from my_project.database import db
 
 
-class Coach(db.Model):
-    __tablename__ = "coach_contact"
+class CoachesContact(db.Model):
+    __tablename__ = "coaches_contact"
+
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    coach_specialization_id = db.Column(db.Integer, db.ForeignKey('coach_specialization.id'))
-    contact_id = db.Column(db.Integer, db.ForeignKey('coach_contact.id'))  # <--- add this
+    phone = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
 
-    contact = db.relationship('CoachesContact', back_populates='coaches')
+    coaches = db.relationship('Coach', back_populates= 'contact')
 
     def to_dict(self):
         return {
             "id": self.id,
             "phone": self.phone,
             "email": self.email,
-            "contact_id": self.contact_id,
         }
