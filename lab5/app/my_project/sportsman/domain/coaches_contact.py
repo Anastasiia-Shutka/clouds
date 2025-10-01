83bd5cf86fb2c7e7c6700f7abf7899c470ef728a
Anastasiia-Shutka <anastasiia.shutka@gmail.com>
2025-10-01 14:26:17 +0300

    whatever

--- lab5/app/my_project/sportsman/domain/coaches_contact.py
+++ lab5/app/my_project/sportsman/domain/coaches_contact.py
@@ -2,7 +2,7 @@ from my_project.database import db
 
 
 class Coach(db.Model):
-    __tablename__ = "coache_contact"
+    __tablename__ = "coach_contact"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
