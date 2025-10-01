6c509f645599dafc7aa0a78036dcd3ff152950ba
Anastasiia-Shutka <anastasiia.shutka@gmail.com>
2025-10-01 13:54:25 +0300

    whatever

--- lab5/app/my_project/sportsman/domain/coach.py
+++ lab5/app/my_project/sportsman/domain/coach.py
@@ -4,6 +4,7 @@ from sqlalchemy import ForeignKey
 
 class Coach(db.Model):
     __tablename__ = "coach"
+    __table_args__ = {'extend_existing': True}
 
 
     id = db.Column(db.Integer, primary_key=True)
