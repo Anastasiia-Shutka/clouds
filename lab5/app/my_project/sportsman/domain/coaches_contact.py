6856529668c1a01a08d49ffa6a992bed1b6d7693
Anastasiia-Shutka <anastasiia.shutka@gmail.com>
2025-10-01 17:07:02 +0300

    whatever

--- lab5/app/my_project/sportsman/domain/coaches_contact.py
+++ lab5/app/my_project/sportsman/domain/coaches_contact.py
@@ -1,4 +1,5 @@
 from my_project.database import db
+from sqlalchemy import ForeignKey
 
 
 class CoachesContact(db.Model):
@@ -8,6 +9,7 @@ class CoachesContact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
+    coach_id = db.Column(db.Integer, ForeignKey('coach.id'), unique=True, nullable=False)
 
     coaches = db.relationship('Coach', back_populates= 'contact')
 
